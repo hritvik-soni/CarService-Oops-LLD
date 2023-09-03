01d65e4b40fa1d34a5b5055d779f719c83997689
Hritvik soni <88422393+hritvik-soni@users.noreply.github.com>
2023-09-03 19:18:38 +0530

    Update coffeeMachine.java

--- coffeeMachine.java
+++ coffeeMachine.java
@@ -1,65 +1,55 @@
 
-/**
- * Created by: Seyhan A. Cileli
- * Date: October 18, 2019
- *
- * "Spread Love!"
- *      - Sey'
- */
+import java.util.*;
 
-import java.util.Scanner;
+class coffeeMachine {
 
-class coffeeMachine1 {
-
-    // having local variables
+  
 
     static int water = 1200, milk = 540, coffeeBeans = 120, disposableCups = 9, money = 550;
     static Scanner input = new Scanner(System.in);
 
-    // espresso method
+
     public static void espresso(int cup) {
 
-        // checking to see if there's enough resource to make coffee
-        // needs 250 water--16 coffee beans -- cost $4
-        if (250 * cup > water) {// checks if there's enough water
+    
+        if (250 * cup > water) {
             System.out.println("Not enough water resource, please refill");
         }
-        if (16 * cup > coffeeBeans) {// checks if there's enough coffee beans
+        if (16 * cup > coffeeBeans) {
             System.out.println("Not enough coffee beans resource, please refill");
         }
-        if (cup > disposableCups) {// checks if there's enough cups
+        if (cup > disposableCups) {
             System.out.println("Not enough cups resource, please refill");
         }
-        // if there's enough resource to make the desire amount
+      
         if (250 * cup < water && 16 * cup < coffeeBeans && cup < disposableCups) {
             System.out.println("Enough resorce, making cup of coffee, please wait.\n");
 
-            // calculating the values, it will subtract from the resources
+       
             water -= 250 * cup;
             coffeeBeans -= 16 * cup;
             disposableCups -= cup;
-            money += 4 * cup;// adding to the value
+            money += 4 * cup;
             remaining();
         }
     }
 
-    // latte method
+
     public static void latte(int cup) {
-        // condition to check resources
-        // needs 350 water -- 75 milk -- 20 coffee beans --cost $7
-        if (350 * cup > water) {// checks if there's enough water
+     
+        if (350 * cup > water) {
             System.out.println("Not enough water resource, please refill");
         }
-        if (75 * cup > milk) {// checks if there's enough milk
+        if (75 * cup > milk) {
             System.out.println("Not enough milk resource, please refill");
         }
-        if (20 * cup > coffeeBeans) {// checks if there's enough coffee beans
+        if (20 * cup > coffeeBeans) {
             System.out.println("Not enough coffee beans resource, please refill");
         }
-        if (cup > disposableCups) {// checks if there's enough disposable cups
+        if (cup > disposableCups) {
             System.out.println("Not enough disposable cups resource, please refill");
         }
-        // if there's enough resource it will make the cup/cups of coffee
+    
         if (350 * cup < water && 75 * cup < milk && 20 * cup < coffeeBeans && cup < disposableCups) {
             System.out.println("Enough resorce, making cup of coffee, please wait.\n");
 
@@ -72,24 +62,22 @@ class coffeeMachine1 {
         }
     }
 
-    // cappuccino method
     public static void cappuccino(int cup) {
 
-        // conditions to check if there's enough resource
-        // needs 200 = water -- 100 = milk -- 12 = coffee beans -- cost $6
-        if (200 * cup > water) {// checks if there's enough water
+     
+        if (200 * cup > water) {
             System.out.println("Not enough water resource, please refill");
         }
-        if (100 * cup > milk) {// checks if there's enough milk
+        if (100 * cup > milk) {
             System.out.println("Not enough milk resource, please refill");
         }
-        if (12 * cup > coffeeBeans) {// checks if there's enough coffee beans
+        if (12 * cup > coffeeBeans) {
             System.out.println("Not enough coffee beans resource, please refill");
         }
-        if (cup > disposableCups) {// checks if there's enough disposable cups
+        if (cup > disposableCups) {
             System.out.println("Not enough cups resource, please refill");
         }
-        // if there's enough resource than it will make the cup/cups of coffee
+    
         if (200 * cup < water && 100 * cup < milk && 12 * cup < coffeeBeans && cup < disposableCups) {
             System.out.println("Enough resource, making cup of coffee, please wait.\n");
 
@@ -102,30 +90,30 @@ class coffeeMachine1 {
         }
     }
 
-    // refill method
+
     public static void fill() {
-        // getting input from user and adding to the value
+      
         System.out.println("How much do you want to refill by?\n");
 
         System.out.println("How much water: ");
-        water += input.nextInt();// Adding to the total of water
+        water += input.nextInt();
 
         System.out.println("How much milk: ");
-        milk += input.nextInt();// adding to the total of milk
+        milk += input.nextInt();
 
         System.out.println("How much coffee beans: ");
-        coffeeBeans += input.nextInt();// adding to the total of coffee beans
+        coffeeBeans += input.nextInt();
 
         System.out.println("How much disposable cups: ");
-        disposableCups += input.nextInt();// adding to the total of disposable cups
+        disposableCups += input.nextInt();
 
         remaining();
 
     }
 
-    // what remains in the values
+
     public static void remaining() {
-        // simple print methods to give what it remains in the coffee machine
+    
         System.out.println("\nResources in the coffee machine");
         System.out.println(water + " Water remaining in the machine");
         System.out.println(milk + " Milk remaining in the machine");
@@ -134,41 +122,41 @@ class coffeeMachine1 {
         System.out.println("$" + money + " Money in the machine\n");
     }
 
-    // buying method
+
     public static void buy() {
 
         System.out.println("What would you like to purchase:");
-        // prompts the user to enter one of the available choices
+    
         System.out.println("1 - espresso\n" + "2 - latee\n" + "3 - cappuccino\n" + "back - Main Menu");
         String choice = input.next();
         int cup;
 
-        if (choice.equals("1")) {// if "1" it fall into the statement and calls the method(espresso)
+        if (choice.equals("1")) {
             System.out.println("How many cups would you like? \n");
-            cup = input.nextInt();// prompts user for the amount he wishes to purchase
+            cup = input.nextInt();
 
             espresso(cup);
 
-        } else if (choice.equals("2")) {// if "2" it fall into the statement and calls the method(latte)
+        } else if (choice.equals("2")) {
             System.out.println("How many cups would you like? ");
-            cup = input.nextInt();// prompts user for the amount he wishes to purchase
+            cup = input.nextInt();
 
             latte(cup);
 
-        } else if (choice.equals("3")) {// if "3" it fall into the statement and calls the method(cappucino)
+        } else if (choice.equals("3")) {
             System.out.println("How many cups would you like? ");
-            cup = input.nextInt();// prompts user for the amount he wishes to purchase
+            cup = input.nextInt();
 
             cappuccino(cup);
-        } else if (!choice.equals("back")) {// if the input is not back it will prompt the question at the start
+        } else if (!choice.equals("back")) {
 
         }
     }
 
-    // removing money from register(i guess)
+ 
     public static void take() {
         System.out.println("Currently you have a balance of $" + money);
-        money -= money;// subtracts all the money available to make it ZERO
+        money -= money;
 
         System.out.println("You have taken money from register your current balance is of $" + money);
     }
@@ -178,22 +166,22 @@ class coffeeMachine1 {
         System.out.println("Welcome please choose what would you like (buy, take, fill, remaining, exit)");
         String choice = input.next();
 
-        while (!choice.equals("exit"))// if the choice doesn't equal exit than the loop continues
+        while (!choice.equals("exit"))
         {
 
-            if (choice.equals("buy")) {// if "buy" than it calls the method(buy)
+            if (choice.equals("buy")) {
                 buy();
-            } else if (choice.equals("take")) {// if "take" than it calls the method(take)
+            } else if (choice.equals("take")) {
                 take();
-            } else if (choice.equals("fill")) {// if "fill" than it calls the method(fill)
+            } else if (choice.equals("fill")) {
                 fill();
-            } else if (choice.equals("remaining")) {// if "remaining" than it calls the method(remaining)
+            } else if (choice.equals("remaining")) {
                 remaining();
-            } else if (!choice.equals("exit")) {// if any other choice is given then it falls under this condition
+            } else if (!choice.equals("exit")) {
                 System.out.println("Wrong input, please try again (buy, take, fill, remaining, exit)\n");
                 choice = input.next();
             }
-            // loops the question to keep asking if the user does't input exit
+          
             System.out.println("\nPlease choose what would you like (buy, take, fill, remaining, exit");
             choice = input.next();
         }
